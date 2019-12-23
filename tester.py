fe1f23b6915b2d409a89fabf120cfee11ffcb772
Ais105 <krissvarcnegger@gmail.com>
2019-12-23 13:33:54 +0300

    k

--- tester.py
+++ tester.py
@@ -1,13 +1,18 @@
+"""
+    [X] repos
+    [X] branches
+    [ ] files for commits
+    [X] tags
+"""
 from github import Github
 import plotly.graph_objects as go
 if __name__ == '__main__':
-    user = input()
-    password = input()
-    g = Github(user,password)
-    print("Get all repos:")
+    # user = input("Enter username: \n")
+    # password = input("Enter password: \n")
+    g = Github("Ais105","m123j54der")
     user = g.get_user()
+    #
     repositories = [repo.name for repo in g.get_user().get_repos()]
-    #get all repos
     fig = go.Figure(data=[go.Table(
         header=dict(values=[user.login],
                     line_color='white',
@@ -18,7 +23,7 @@ if __name__ == '__main__':
                    fill_color='rgb(230, 204, 255)',
                    align='center'))
     ])
-    fig.update_layout(width=400, height=10000)
+    fig.update_layout(width=500, height=10000)
     fig.show()
     #get all branches
     repository_name = input()
@@ -37,45 +42,47 @@ if __name__ == '__main__':
                    fill_color='rgb(230, 204, 255)',
                    align='center'))
     ])
-    fig.update_layout(width=300, height=300)
+    fig.update_layout(width=400, height=500)
     fig.show()
-    # tags = [g.get_user().get_repo(repository_name).get_commit(sha="7d08aedbab9e735274e0426d96b2b67d83817781")]
-    # fig = go.Figure(data=[go.Table(
-    #         header=dict(values=[repository_name],
-    #                     line_color='white',
-    #                     fill_color='rgb(206, 153, 255)',
-    #                     align='center'),
-    #         cells=dict(values=[tags],  # 1nd column
-    #                    line_color='white',
-    #                    fill_color='rgb(230, 204, 255)',
-    #                    align='center'))
-    #     ])
-    # fig.update_layout(width=300, height=300)
-    # fig.show()
+    tags = g.get_user().get_repo(repository_name).get_tags()
+    tags_id_list = []
+    for tag in tags:
+        tags_id_list.append(tag.name)
+    fig = go.Figure(data=[go.Table(
+            header=dict(values=[repository_name],
+                        line_color='white',
+                        fill_color='rgb(206, 153, 255)',
+                        align='center'),
+            cells=dict(values=[tags_id_list],  # 1nd column
+                       line_color='white',
+                       fill_color='rgb(230, 204, 255)',
+                       align='center'))
+        ])
+    fig.update_layout(width=500, height=130000)
+    fig.show()
+    # commits
     #branch_name = input()
-    # print("Get all commits in branch %s:" % branch_name)
+    print("Get all commits in repository %s:" % repository_name)
     # if branch_name not in branches:
     #     print("Input branch doesn't exist")
-    #branch = g.get_user().get_repo(repository_name).get_branch(branch_name)
-    #comments = [commit.message for commit in branch.commit]
-    # fig = go.Figure(data=[go.Table(
-    #     header=dict(values=[branch_name],
-    #                 line_color='white',
-    #                 fill_color='rgb(206, 153, 255)',
-    #                 align='center'),
-    #     cells=dict(values=[comments],  # 1nd column
-    #                line_color='white',
-    #                fill_color='rgb(230, 204, 255)',
-    #                align='center'))
-    # ])
-    # fig.update_layout(width=400, height=10000)
-    # fig.show()
+    commits = g.get_user().get_repo(repository_name).get_commits()
+    last = commits[0]
+    commit_list = [last.files]
+    # for comm in commits:
+    #     commit_list.append(commits.commit.message)
+    fig = go.Figure(data=[go.Table(
+        header=dict(values=[repository_name],
+                    line_color='white',
+                    fill_color='rgb(206, 153, 255)',
+                    align='center'),
+        cells=dict(values=[commit_list],  # 1nd column
+                   line_color='white',
+                   fill_color='rgb(230, 204, 255)',
+                   align='center'))
+    ])
+    fig.update_layout(width=400, height=10000)
+    fig.show()
     # print("<--------------------------------------------------->")
-
-    # print("Get last commit message in repo %s:" % repository_name)
-    # branch = g.get_user().get_repo(repository_name).get_branch("master")
-    # lastCommit = branch.commit.value.commit
-    # print("\t%s" % lastCommit.message.value)
-
-    print("<--------------------------------------------------->")
-    #получить список всех тегов;
+    #список субмодулей
+    #список файлов для коммита
+    #количество багов для измененных файлов
